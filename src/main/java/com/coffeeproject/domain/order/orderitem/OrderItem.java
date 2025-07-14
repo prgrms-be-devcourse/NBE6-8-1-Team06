e0f3207c3bad7b016eb9341fc7b008714ae13b5b
shihan00321 <shihan001@naver.com>
2025-07-15 01:06:36 +0900

    OrderItem 엔티티 정의

---
+++ src/main/java/com/coffeeproject/domain/order/orderitem/OrderItem.java
@@ -0,0 +1,26 @@
+package com.coffeeproject.domain.order.orderitem;
+
+import com.coffeeproject.domain.order.order.entity.Order;
+import com.coffeeproject.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderItem extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+    // TODO : 상품 엔티티 @ManyToOne 연관관계 설정 필요
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+}
