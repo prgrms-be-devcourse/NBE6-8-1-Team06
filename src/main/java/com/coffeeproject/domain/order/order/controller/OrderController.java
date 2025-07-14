cb248d646716cc7ee973b3b69136b868d0d7d200
shihan00321 <shihan001@naver.com>
2025-07-15 01:53:13 +0900

    주문 생성 메서드 반환 타입 변경

--- src/main/java/com/coffeeproject/domain/order/order/controller/OrderController.java
+++ src/main/java/com/coffeeproject/domain/order/order/controller/OrderController.java
@@ -1,11 +1,11 @@
 package com.coffeeproject.domain.order.order.controller;
 
 import com.coffeeproject.domain.order.order.dto.OrderRequest;
+import com.coffeeproject.domain.order.order.dto.OrderResponse;
 import com.coffeeproject.domain.order.order.entity.Order;
 import com.coffeeproject.domain.order.order.service.OrderService;
 import com.coffeeproject.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +18,12 @@ public class OrderController {
      private final OrderService orderService;
 
      @PostMapping
-     public RsData<Void> createOrder(@RequestBody OrderRequest request) {
+     public RsData<OrderResponse> createOrder(@RequestBody OrderRequest request) {
          Order order = orderService.createOrder(request);
          return new RsData<>(
                  "200",
-                 "%d번 주문이 생성되었습니다.".formatted(order.getId())
+                 "%d번 주문이 생성되었습니다.".formatted(order.getId()),
+                 new OrderResponse(order)
          );
      }
 }
