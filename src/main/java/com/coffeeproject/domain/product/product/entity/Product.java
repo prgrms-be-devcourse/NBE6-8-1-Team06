52c94716d07bc3f4702b9ad5ea4051e179cf9d94
moon472907 <moon472907@gmail.com>
2025-07-14 17:49:02 +0900

    aaaa

---
+++ src/main/java/com/coffeeproject/domain/product/product/entity/Product.java
@@ -0,0 +1,19 @@
+package com.coffeeproject.domain.product.product.entity;
+
+import com.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product extends BaseEntity {
+
+    private String author;
+    private String name;
+    private String description;
+    private int price;
+    private String imageUrl;
+}
+
---
+++ src/main/java/com/coffeeproject/domain/product/product/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.coffeeproject.domain.product.product.repository;
+
+import com.domain.product.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+}
