c39b8d7bfa9cff4cddcab5adf8572931d061ab45
moon472907 <moon472907@gmail.com>
2025-07-15 10:27:16 +0900

    Product Api 작성

--- src/main/java/com/coffeeproject/domain/product/product/api/ApiV1ProductController.java
+++ src/main/java/com/coffeeproject/domain/product/product/api/ApiV1ProductController.java
@@ -3,11 +3,37 @@ package com.coffeeproject.domain.product.product.api;
 import com.coffeeproject.domain.product.product.entity.Product;
 import com.coffeeproject.domain.product.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ApiV1ProductController {
     private final ProductService productService;
+
+
+    @PostMapping
+    @Transactional
+    public Product createProduct(Product product) {
+        return productService.save(product);
+    }
+    @GetMapping
+    @Transactional
+    public List<Product> getAllProducts()
+    {
+        return productService.findAll();
+    }
+
+    @GetMapping({"/{id}"})
+    @Transactional
+    public Product getProductById(int id) {
+        return productService.findById(id).orElse(null);
+    }
+}
+
--- src/main/java/com/coffeeproject/domain/product/product/service/ProductService.java
+++ src/main/java/com/coffeeproject/domain/product/product/service/ProductService.java
@@ -1,13 +1,29 @@
 package com.coffeeproject.domain.product.product.service;
 
 
+import com.coffeeproject.domain.product.product.entity.Product;
 import com.coffeeproject.domain.product.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 public final ProductRepository productRepository;
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(int id) {
+        return productRepository.findById(id);
+    }
 
 }
