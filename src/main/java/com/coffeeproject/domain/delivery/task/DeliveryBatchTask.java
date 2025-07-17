9849c9abdf6421a3057dbbc22ac2a1c9bd472fa9
jiseopshin <wltjq1203@icloud.com>
2025-07-17 09:40:32 +0900

    배치작업 클래스 작성완료

--- src/main/java/com/coffeeproject/domain/delivery/task/DeliveryBatchTask.java
+++ src/main/java/com/coffeeproject/domain/delivery/task/DeliveryBatchTask.java
@@ -13,27 +13,31 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 스케줄러를 통해 수행될 작업을 정의하는 클래스로 Runnable을 구현합니다.
  */
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DeliveryBatchTask implements Runnable {
     private final DeliveryService deliveryService;
     private final OrderService orderService;
 
+    record key(String eMail, String address) {}
+
     @Override
     public void run() {
-        log.info("스케줄링 작업 실행: {}", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        //1. 단 배송해야할 오더의 리스트를 반환한다.
-        List<Order> list = orderService.getAllOrders().stream()
+        Map<key, List<Order>> orders = orderService.getAllOrders().stream()
                 .filter(order -> order.getStatus() == OrderStatus.PAID)
                 .filter(order -> order.getCreateDate().isAfter(LocalDateTime.now().minusHours(24)))
-                .toList();
-        //2. 배송 엔티티로 만들어 저장한다.
-        //FIXME : 이건 잘못된 사용법. 주문자와 주소가 동일한 Order끼리만 리스트로 만들어서 생성해야함
-        Delivery delivery = deliveryService.createDelivery(list);
+                .collect(Collectors.groupingBy(
+                        order -> new key(order.getCustomerEmail(), order.getShippingAddress())
+                ));
+
+        for (List<Order> order : orders.values()) {
+            deliveryService.createDelivery(order);
+        }
     }
 }
