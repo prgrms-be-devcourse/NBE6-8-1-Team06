bacd620bd023239e1b7f1d447dc4e312d3c5cacc
jiseopshin <wltjq1203@icloud.com>
2025-07-17 11:36:52 +0900

    오타수정

--- src/main/java/com/coffeeproject/domain/delivery/api/ApiV1DeliverySchedulerController.java
+++ src/main/java/com/coffeeproject/domain/delivery/api/ApiV1DeliverySchedulerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/scheduler")
+@RequestMapping("/api/v1/scheduler")
 @RequiredArgsConstructor
 public class ApiV1DeliverySchedulerController {
     private final DeliverySchedulerService schedulerControlService;
